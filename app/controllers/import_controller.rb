3f2fc41cac9061335fa5724cd67f7f4ac42f4232
Austin <austinlandry271@gmail.com>
2025-02-02 23:57:16 +1100

    Forgot to remove these print statements

--- app/controllers/import_controller.rb
+++ app/controllers/import_controller.rb
@@ -1,8 +1,6 @@
 class ImportController < ApplicationController
     def csv_import
         file_path = params[:file].path
-        # print("printing the file path now! \n")
-        # print(file_path + "\n")
         CsvImporter.import(file_path)
         render json: { message: 'Import successful' }, status: :ok
     
